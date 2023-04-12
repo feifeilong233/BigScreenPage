d2376ffbb75109a6f19dd3b8c77c6cd45a8eec6b
feilong <2529121135@qq.com>
2023-04-12 18:12:35 +0800

    fix 预览模型

--- dt-test-frontend/src/views/simulation/modelView/comps.vue
+++ dt-test-frontend/src/views/simulation/modelView/comps.vue
@@ -16,8 +16,7 @@
       <div id="three-frame"></div>
       <div class="chartsdoms_cons_rights space-y-2 w-22vw">
         <div class="box1 enter-x-r w-22vw">
-          <div class="title1">测试指数</div>
-          <div class="h-40vh box-body">
+          <a-card class="h-45vh" title="测试指数" size="small">
             <a-row v-for="(item, index) in roadValue" :key="index" type="flex" align="middle">
               <a-col :span="4">
                 <span>road_{{index}}</span>
@@ -32,16 +31,15 @@
                     :min="0"
                     :max="0.5"
                     :step="0.01"
-                    style="margin-left: 16px"
                 />
               </a-col>
             </a-row>
-          </div>
+          </a-card>
         </div>
         <div class="box1 enter-x-r w-22vw">
-          <div class="title1">测试指数</div>
+          <div class="title1">路面材料</div>
           <div class="h-40vh">
-
+            <div id="container3D"></div>
           </div>
         </div>
       </div>
@@ -289,12 +287,14 @@ const model_load = (H) => {
   width: 100%;
   height: 10px;
 
-  background: linear-gradient(to right,
-  transparent,
-  rgba(251, 191, 36, 0.3),
-  transparent,
-  rgba(251, 191, 36, 0.3),
-  transparent);
+  background: linear-gradient(
+      to right,
+      transparent,
+      rgba(6, 141, 193, 0.3),
+      transparent,
+      rgba(6, 141, 193, 0.3),
+      transparent
+  );
   transform: translateY(-100px);
   animation: doms 0.5s linear forwards;
   animation-delay: 0.7s;
@@ -308,13 +308,11 @@ const model_load = (H) => {
   left: 0;
   width: 100%;
   height: 40px;
-
   .titlbox {
     width: 28rem;
     height: 100%;
     overflow: hidden;
     position: relative;
-
     & > div {
       position: absolute;
       width: 100%;
@@ -324,7 +322,7 @@ const model_load = (H) => {
       &:nth-of-type(1) {
         width: 80%;
         left: 10%;
-        border: 100px solid rgba(251, 191, 36, 0.3);
+        border: 100px solid rgba(6, 141, 193, 0.3);
         border-left: 80px solid transparent;
         border-right: 80px solid transparent;
         transform: translateY(-100px);
@@ -333,11 +331,10 @@ const model_load = (H) => {
         opacity: 0;
         z-index: 9;
       }
-
       &:nth-of-type(2) {
         width: 90%;
         left: 5%;
-        border: 100px solid rgba(251, 191, 36, 0.2);
+        border: 80px solid rgba(6, 141, 193, 0.2);
         border-left: 80px solid transparent;
         border-right: 80px solid transparent;
         transform: translateY(-100px);
@@ -345,10 +342,9 @@ const model_load = (H) => {
         animation-delay: 0.3s;
         opacity: 0;
       }
-
       &:nth-of-type(3) {
         width: 100%;
-        border: 100px solid rgba(251, 191, 36, 0.1);
+        border: 100px solid rgba(6, 141, 193, 0.1);
         border-left: 140px solid transparent;
         border-right: 140px solid transparent;
         transform: translateY(-100px);
@@ -356,7 +352,6 @@ const model_load = (H) => {
         animation-delay: 0.4s;
         opacity: 0;
       }
-
       &:nth-of-type(4) {
         width: 100%;
         text-align: center;
@@ -369,17 +364,19 @@ const model_load = (H) => {
         animation: doms 0.5s linear forwards;
         font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
         'Lucida Sans', Arial, sans-serif;
+        // background: url('../../../../assets/header.png');
       }
-
       &:nth-of-type(5) {
         width: 80%;
         left: 10%;
         height: 10px;
         top: 38px;
-        background: linear-gradient(to right,
-        transparent,
-        rgba(115, 208, 245, 0.7),
-        transparent);
+        background: linear-gradient(
+            to right,
+            transparent,
+            rgba(115, 208, 245, 0.7),
+            transparent
+        );
         z-index: 9;
       }
     }
@@ -396,24 +393,21 @@ const model_load = (H) => {
 .echartsdoms {
   width: 100%;
   height: 100%;
-
   .maobg {
     background: rgba(193, 189, 189, 0.1);
     backdrop-filter: blur(4px);
   }
-
   .title1 {
-    color: #c9dcf5;
+    color: rgba(0, 0, 0, 0.85);
     border-bottom: 2px solid #525c66;
     padding: 0.3rem 0.5rem;
     position: relative;
     font-size: 0.8rem;
     font-weight: bold;
-
     &::before {
       content: '';
       display: inline-block;
-      background: rgba(251, 191, 36, 1);
+      background: #8ce1fb;
       width: 3px;
       height: 100%;
       position: absolute;
@@ -427,14 +421,9 @@ const model_load = (H) => {
       color: #c9dcf5;
       font-size: 8px;
     }
-
     .chartsdoms_cons_rights {
       .box1 {
         .maobg;
-
-        .box-body {
-          padding: 24px;
-        }
       }
     }
   }
